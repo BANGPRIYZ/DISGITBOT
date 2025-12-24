95db9147bc9a2dd6c5eb0a6f8b585b281ae8c798
tim48-robot <justintimothy303@gmail.com>
2025-12-25 00:51:06 +0700

    my bini??

--- TP1.java
+++ TP1.java
@@ -2,5 +2,6 @@ class TP1{
     public static void main(String[] args){
         System.out.println("KEREN");
         System.out.println("Test MY ORGANIZATION BOT")
+        System.out.println("Pgn punya my bini")
     }
 }
